b49948fc403dc1a64334774e97ce415eea15e399
bridgemami <mdbridgeman@gmail.com>
2022-12-12 18:26:56 -0800

    cleaned up [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -28,26 +28,27 @@ export async function getStaticProps({ params }) {
 }
 
 
-// make a react component to display all details about a person when a dynamic route matches, like id 1 or id 2
+// make a react component to display all details about type when a dynamic route matches, like id 1 or id 2
 export default function Entry({ itemData }) {
   let basic = itemData[0].basic_information.split(',');
   console.log(`%c[id]: Basic information: %c${basic}`, 'color: yellow', "color: red');");
+  console.log(` ${basic[1].replaceAll('_', ' ')}`)
   return (
     <>
     <h1 className='text-center mt-2'>Details</h1>
       <article className="container border border-secondary pb-3 ps-4">
         <section className="row">
           <h5 className="col-12 pt-2 ps-2">{(itemData[0].post_type.charAt(0).toUpperCase())+(itemData[0].post_type.slice(1))} ID: {itemData[0].ID}</h5>
-          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[0]}} />
-          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[1]}} />
-          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[2]}} />
-          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[3]}} />
+          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[0].replaceAll('_', '  ')}} />
+          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[1].replaceAll('_', '  ')}} />
+          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[2].replaceAll('_', '  ')}} />
+          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[3].replaceAll('_', '  ')}} />
           <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[4]}} />
-          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[5]}} />
+          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[5].replaceAll('_', '  ')}} />
           <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[6]}} />
-          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[7]}} />
+          <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[7].replaceAll('_', '  ')}} />
           <p className="card-text"  dangerouslySetInnerHTML={{__html: basic[8]}} />
-          </section>
+          </section> 
           <button type="button" className="btn btn-secondary width_10 mt-3">
             <Link href='/'>
             <a>Back to Homepage</a>
